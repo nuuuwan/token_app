32a2e8e1bf986efd97ce747c6e7c0a70492217e4
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-29 16:32:40 +0530

    Added InputPriority

---
+++ src/view/molecules/InputPriority.js
@@ -0,0 +1,30 @@
+import LabelledTextInput from "../../view/molecules/LabelledTextInput";
+import Box from "@mui/material/Box";
+import Alert from "@mui/material/Alert";
+import Validate from "../../nonview/core/Validate";
+
+export default function InputPriority({ selectedPriority, onChangePriority }) {
+  let severity, alertText;
+  if (selectedPriority.toString().trim().length === 0) {
+    severity = "error";
+    alertText = "A priority value is required to generate a token.";
+  } else if (Validate.priority(selectedPriority)) {
+    severity = "success";
+    alertText = "Looks good!";
+  } else {
+    severity = "error";
+    alertText =
+      "A valid priority value must be an number between 1 and 10,000.";
+  }
+
+  return (
+    <Box>
+      <LabelledTextInput
+        label="Priority"
+        text={selectedPriority}
+        onChange={onChangePriority}
+      />
+      <Alert severity={severity}>{alertText}</Alert>
+    </Box>
+  );
+}
