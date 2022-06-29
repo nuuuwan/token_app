4e9c94bd4597916d9b84cc5b9f801ad935c69de3
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-29 12:14:35 +0530

    Added CRYPTO_KEY_TYPE, getKeyPairFromLocalStorage, setKeyPairToLocalStorage, createToken (INCOMPLETE)

--- src/nonview/base/Crypto.js
+++ src/nonview/base/Crypto.js
@@ -1,6 +1,26 @@
 const nacl = require("tweetnacl");
 
+const LOCAL_STORAGE_KEY = "token-app-key-pair";
+
+export const CRYPTO_KEY_TYPE = {
+  PUBLIC: "public",
+  SECRET: "secret",
+};
+
 export default class Crypto {
+  static getKeyPairFromLocalStorage() {
+    const dataJSON = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (dataJSON) {
+      return JSON.parse(dataJSON);
+    }
+    return null;
+  }
+
+  static setKeyPairToLocalStorage(keyPair) {
+    const dataJSON = JSON.stringify(keyPair);
+    localStorage.setItem(LOCAL_STORAGE_KEY, dataJSON);
+  }
+
   static getKeyPair() {
     const raw = nacl.box.keyPair();
     return {
@@ -17,5 +37,13 @@ export default class Crypto {
   static deserializeKey(s) {
     const uInt8ArrayKey = JSON.parse(window.atob(s));
     return uInt8ArrayKey;
+  }
+
+  static createToken(payload) {
+    return {
+      payload,
+      publicKey: "XXX",
+      payloadHash: "XXX",
+    };
   }
 }
