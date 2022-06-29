25fa0b3f69acf58d06951b6d6fde1f017e8f6a69
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-29 17:31:36 +0530

    Formatting

--- src/view/molecules/TrustedSourceView.js
+++ src/view/molecules/TrustedSourceView.js
@@ -1,20 +1,26 @@
 import Alert from "@mui/material/Alert";
-import Typography from "@mui/material/Typography";
-import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
 import TrustedSource from "../../nonview/core/TrustedSource";
 
-import AlignCenter from "../../view/atoms/AlignCenter";
-
 export default function TrustedSourceView({ publicKey }) {
-  const trustedSource = TrustedSource.getFromPublicKey(publicKey);
-  if (!trustedSource) {
-    return <Alert severity="error">Issued by an unknown source</Alert>;
+  if (!publicKey) {
+    return null;
   }
-  return (
-    <AlignCenter>
-      <Typography color="green">{"Issued by " + trustedSource.name}</Typography>
-      <CheckCircleIcon sx={{ color: "green", fontSize: "100%" }} />
-    </AlignCenter>
-  );
+  const trustedSource = TrustedSource.getFromPublicKey(publicKey);
+
+  let severity, message;
+  if (trustedSource) {
+    severity = "success";
+    message = (
+      <>
+        Trusted Source
+        <strong>{" " + trustedSource.name}</strong>
+      </>
+    );
+  } else {
+    severity = "error";
+    message = "Untrusted Source";
+  }
+
+  return <Alert severity={severity}>{message}</Alert>;
 }
