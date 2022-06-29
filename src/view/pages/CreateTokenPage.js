6a742c66a5a6f07c8c8190dd669909372b77419d
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-29 16:20:19 +0530

    Use inputs

--- src/view/pages/CreateTokenPage.js
+++ src/view/pages/CreateTokenPage.js
@@ -7,13 +7,23 @@ import NoteAddIcon from "@mui/icons-material/NoteAdd";
 import Crypto from "../../nonview/base/Crypto";
 import TimeX, { SECONDS_IN } from "../../nonview/base/TimeX";
 import URLContext from "../../nonview/base/URLContext";
+import InputVehicleNumber from "../../view/molecules/InputVehicleNumber";
 
 export default class CreateTokenPage extends Component {
-  onClickCreateToken() {
-    const currentTimeUT = TimeX.getUnixTime();
-    const token = Crypto.encryptToken({
+  constructor(props) {
+    super(props);
+    this.state = {
       vehicleNumber: "ABC-1234",
       priority: 12,
+    };
+  }
+
+  onClickCreateToken() {
+    const { vehicleNumber, priority } = this.state;
+    const currentTimeUT = TimeX.getUnixTime();
+    const token = Crypto.encryptToken({
+      vehicleNumber,
+      priority,
       timeCreatedUT: currentTimeUT,
       timeExpiryUT: currentTimeUT + SECONDS_IN.DAY,
     });
@@ -25,9 +35,18 @@ export default class CreateTokenPage extends Component {
     window.location.reload(true);
   }
 
+  onChangeVehicleNumber(vehicleNumber) {
+    this.setState({ vehicleNumber });
+  }
+
   render() {
+    const { vehicleNumber, priority } = this.state;
     return (
       <Stack spacing={2}>
+        <InputVehicleNumber
+          selectedVehicleNumber={vehicleNumber}
+          onChangeVehicleNumber={this.onChangeVehicleNumber.bind(this)}
+        />
         <Button
           onClick={this.onClickCreateToken.bind(this)}
           variant="contained"
