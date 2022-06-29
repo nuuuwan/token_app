b7060b85df0b9a8c6bfed1a3c9bae9c385971a5f
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-29 16:32:50 +0530

    Use Validate

--- src/view/molecules/InputVehicleNumber.js
+++ src/view/molecules/InputVehicleNumber.js
@@ -1,33 +1,7 @@
 import LabelledTextInput from "../../view/molecules/LabelledTextInput";
 import Box from "@mui/material/Box";
 import Alert from "@mui/material/Alert";
-
-function isLetter(c) {
-  return c.toLowerCase() !== c.toUpperCase();
-}
-
-function isValid(vehicleNumber) {
-  const tokens = vehicleNumber.split("-");
-  if (tokens.length !== 2) {
-    return false;
-  }
-
-  if (![2, 3].includes(tokens[0].length)) {
-    return false;
-  }
-  if (!isLetter(tokens[0])) {
-    return false;
-  }
-
-  if (tokens[1].length !== 4) {
-    return false;
-  }
-  if (!Number.isInteger(parseInt(tokens[1]))) {
-    return false;
-  }
-
-  return true;
-}
+import Validate from "../../nonview/core/Validate";
 
 export default function InputVehicleNumber({
   selectedVehicleNumber,
@@ -37,7 +11,7 @@ export default function InputVehicleNumber({
   if (selectedVehicleNumber.trim().length === 0) {
     severity = "error";
     alertText = "A valid vehicle number is required to generate a token.";
-  } else if (isValid(selectedVehicleNumber)) {
+  } else if (Validate.vehicleNumber(selectedVehicleNumber)) {
     severity = "success";
     alertText = "Looks good!";
   } else {
