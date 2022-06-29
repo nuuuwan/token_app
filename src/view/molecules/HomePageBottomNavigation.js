a5c3f07f085d42cf8c0c4ea8bd0d4ef0a9585d11
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-29 12:15:13 +0530

    Added onClickOpenPage, added page links

--- src/view/molecules/HomePageBottomNavigation.js
+++ src/view/molecules/HomePageBottomNavigation.js
@@ -3,22 +3,25 @@ import React from "react";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import Paper from "@mui/material/Paper";
-import Typography from "@mui/material/Typography";
+import KeyIcon from "@mui/icons-material/Key";
+import NoteAddIcon from "@mui/icons-material/NoteAdd";
 import RefreshIcon from "@mui/icons-material/Refresh";
 
-import I18N, { LANG_LIST } from "../../nonview/base/I18N";
 import URLContext from "../../nonview/base/URLContext";
-import { ENT_ALL } from "../../nonview/core/Ent";
 
 export default function HomePageBottomNavigation({
   timeLatestRefresh,
   onSelectLanguage,
 }) {
   const onClickRefresh = function () {
-    let context = URLContext.getContext();
-    context.ent = ENT_ALL;
-    URLContext.setContext(context);
     window.location.reload(true);
+  };
+
+  const onClickOpenPage = function (page) {
+    let context = URLContext.getContext();
+    context.page = page;
+    URLContext.setContext(context);
+    onClickRefresh();
   };
 
   return (
@@ -27,27 +30,19 @@ export default function HomePageBottomNavigation({
       elevation={3}
     >
       <BottomNavigation showLabels>
-        {LANG_LIST.map(function (lang) {
-          const onClickInner = function () {
-            onSelectLanguage(lang.lang);
-          };
-          const color = lang.lang === I18N.getLang() ? lang.color : "lightgray";
-
-          return (
-            <BottomNavigationAction
-              key={"button-lang-" + lang.lang}
-              label={
-                <Typography variant="h6" color={color}>
-                  {lang.shortLabel}
-                </Typography>
-              }
-              onClick={onClickInner}
-            />
-          );
-        })}
         <BottomNavigationAction
           icon={<RefreshIcon />}
           onClick={onClickRefresh}
+        />
+        <BottomNavigationAction
+          label="Crypto Keys"
+          icon={<KeyIcon />}
+          onClick={() => onClickOpenPage("cryptoKeys")}
+        />
+        <BottomNavigationAction
+          label="Create Token"
+          icon={<NoteAddIcon />}
+          onClick={() => onClickOpenPage("createToken")}
         />
       </BottomNavigation>
     </Paper>
