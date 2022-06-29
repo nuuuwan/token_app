f4734cf684202f569ef90b605ded3b3c8d63bcd4
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-29 17:31:29 +0530

    Styles

--- src/view/molecules/LabelledTextInput.js
+++ src/view/molecules/LabelledTextInput.js
@@ -2,16 +2,22 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 
-export default function LabelledTextInput({ label, text, onChange }) {
+export default function LabelledTextInput({
+  label,
+  text,
+  onChange,
+  multiline,
+  color,
+}) {
   const onChangeInner = function (e) {
     onChange(e.target.value);
   };
   return (
     <Box sx={{ p: 0.5 }}>
-      <Typography variant="subtitle1" sx={{ fontSize: "67%" }}>
+      <Typography variant="subtitle1" sx={{ fontSize: "67%", color }}>
         {label}
       </Typography>
-      <TextField value={text} onChange={onChangeInner} />
+      <TextField value={text} onChange={onChangeInner} multiline />
     </Box>
   );
 }
