0fd47aca0074232029ad1b28a0c162ff892fc8de
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-29 15:30:22 +0530

    Added payload content

--- src/view/molecules/TokenView.js
+++ src/view/molecules/TokenView.js
@@ -1,12 +1,30 @@
+import QRCode from "react-qr-code";
+
+import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
-const STYLE_TOKEN = {
-  wordBreak: "break-all",
-};
+import TimeX from "../../nonview/base/TimeX";
 
-export default function TokenView({ token }) {
-  if (!token) {
-    return null;
-  }
-  return <Typography sx={STYLE_TOKEN}>{JSON.stringify(token)}</Typography>;
+import LabelledText from "../../view/molecules/LabelledText";
+
+export default function TokenView({ payload, url }) {
+  const isExpired = payload.timeExpiryUT < TimeX.getUnixTime();
+  const color = isExpired ? "red" : "darkgreen";
+  return (
+    <Box sx={{ m: 1, p: 3 }}>
+      <Typography variant="h6">Token</Typography>
+      <QRCode value={url} size={200} />
+
+      <LabelledText label="Vehicle Number" text={payload.vehicleNumber} />
+      <LabelledText
+        label="Time Created"
+        text={TimeX.formatTime(payload.timeCreatedUT)}
+      />
+      <LabelledText
+        label="Time Expiry"
+        text={TimeX.formatTime(payload.timeExpiryUT)}
+        color={color}
+      />
+    </Box>
+  );
 }
