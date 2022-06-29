f75b75c0f3c6b7c4ad9533eea6fadc7f77a04949
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-29 15:53:54 +0530

    Updated LabelledText format

--- src/view/molecules/LabelledText.js
+++ src/view/molecules/LabelledText.js
@@ -1,14 +1,15 @@
+import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
 export default function LabelledText({ label, text, color }) {
   return (
-    <>
+    <Box sx={{ p: 0.5 }}>
       <Typography variant="subtitle1" sx={{ fontSize: "67%" }}>
         {label}
       </Typography>
-      <Typography variant="body1" color={color}>
+      <Typography variant="body1" sx={{ fontSize: "133%", color }}>
         {text}
       </Typography>
-    </>
+    </Box>
   );
 }
