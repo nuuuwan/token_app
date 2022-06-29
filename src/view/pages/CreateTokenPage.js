89a0cdc4d8e6ef5c2d2010092154d741bf76152b
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-29 16:32:58 +0530

    Use InputPriority and Validate

--- src/view/pages/CreateTokenPage.js
+++ src/view/pages/CreateTokenPage.js
@@ -8,13 +8,15 @@ import Crypto from "../../nonview/base/Crypto";
 import TimeX, { SECONDS_IN } from "../../nonview/base/TimeX";
 import URLContext from "../../nonview/base/URLContext";
 import InputVehicleNumber from "../../view/molecules/InputVehicleNumber";
+import InputPriority from "../../view/molecules/InputPriority";
+import Validate from "../../nonview/core/Validate";
 
 export default class CreateTokenPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      vehicleNumber: "ABC-1234",
-      priority: 12,
+      vehicleNumber: "",
+      priority: "",
     };
   }
 
@@ -39,18 +41,30 @@ export default class CreateTokenPage extends Component {
     this.setState({ vehicleNumber });
   }
 
+  onChangePriority(priority) {
+    this.setState({ priority });
+  }
+
   render() {
     const { vehicleNumber, priority } = this.state;
+    const areAllValid =
+      Validate.vehicleNumber(vehicleNumber) && Validate.priority(priority);
+
     return (
       <Stack spacing={2}>
         <InputVehicleNumber
           selectedVehicleNumber={vehicleNumber}
           onChangeVehicleNumber={this.onChangeVehicleNumber.bind(this)}
         />
+        <InputPriority
+          selectedPriority={priority}
+          onChangePriority={this.onChangePriority.bind(this)}
+        />
         <Button
           onClick={this.onClickCreateToken.bind(this)}
           variant="contained"
           startIcon={<NoteAddIcon />}
+          disabled={!areAllValid}
         >
           Create Token
         </Button>
