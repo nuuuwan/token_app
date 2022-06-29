aee0ab40c69a3ef88421c4dc51c50dfa9f8273e5
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-29 15:54:08 +0530

    Changed formatting

--- src/view/molecules/TokenView.js
+++ src/view/molecules/TokenView.js
@@ -2,29 +2,37 @@ import QRCode from "react-qr-code";
 
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
 
 import TimeX from "../../nonview/base/TimeX";
+import TrustedSourceView from "../../view/molecules/TrustedSourceView";
 
 import LabelledText from "../../view/molecules/LabelledText";
 
-export default function TokenView({ payload, url }) {
+export default function TokenView({ payload, publicKey, url }) {
   const isExpired = payload.timeExpiryUT < TimeX.getUnixTime();
   const color = isExpired ? "red" : "darkgreen";
   return (
     <Box sx={{ m: 1, p: 3 }}>
-      <Typography variant="h6">Token</Typography>
-      <QRCode value={url} size={200} />
+      <Stack spacing={1}>
+        <Typography variant="h4">Token</Typography>
+        <TrustedSourceView publicKey={publicKey} />
+        <QRCode value={url} />
+        <Box>
+          <LabelledText label="Vehicle Number" text={payload.vehicleNumber} />
+          <LabelledText label="Priority" text={payload.priority} />
+          <LabelledText
+            label="Time Expiry"
+            text={TimeX.formatTime(payload.timeExpiryUT)}
+            color={color}
+          />
 
-      <LabelledText label="Vehicle Number" text={payload.vehicleNumber} />
-      <LabelledText
-        label="Time Created"
-        text={TimeX.formatTime(payload.timeCreatedUT)}
-      />
-      <LabelledText
-        label="Time Expiry"
-        text={TimeX.formatTime(payload.timeExpiryUT)}
-        color={color}
-      />
+          <LabelledText
+            label="Time Created"
+            text={TimeX.formatTime(payload.timeCreatedUT)}
+          />
+        </Box>
+      </Stack>
     </Box>
   );
 }
