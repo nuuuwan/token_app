d6baf6a0e77a8748b564f32f5303b051eed03674
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-30 09:37:38 +0530

    [Render Recent Tokens] Added "Short" mode to token list page.

--- src/view/molecules/TokenView.js
+++ src/view/molecules/TokenView.js
@@ -1,5 +1,5 @@
 import QRCode from "react-qr-code";
-
+import Card from "@mui/material/Card";
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
@@ -8,31 +8,62 @@ import TimeX from "../../nonview/base/TimeX";
 
 import LabelledText from "../../view/molecules/LabelledText";
 import TrustedSourceView from "../../view/molecules/TrustedSourceView";
+import IconText from "../../view/molecules/IconText";
 
-export default function TokenView({ tokenInfo }) {
+export default function TokenView({ tokenInfo, short }) {
   const { payload, publicKey, url } = tokenInfo;
   const isExpired = payload.timeExpiryUT < TimeX.getUnixTime();
   const color = isExpired ? "red" : "darkgreen";
-  return (
-    <Stack spacing={1} sx={{ m: 1, p: 3 }}>
-      <Typography variant="h4">Token</Typography>
-      <TrustedSourceView publicKey={publicKey} />
-      <QRCode value={url} />
-      <Box>
-        <LabelledText label="Vehicle Number" text={payload.vehicleNumber} />
-        <LabelledText label="Priority" text={payload.priority} />
-        <LabelledText
-          label="Time Expiry"
-          text={TimeX.formatTime(payload.timeExpiryUT)}
-          color={color}
-        />
 
-        <LabelledText
-          label="Time Created"
-          text={TimeX.formatTime(payload.timeCreatedUT)}
-        />
-        <LabelledText label="Public Key" text={publicKey} />
-      </Box>
-    </Stack>
-  );
+  const onClick = function () {
+    window.history.pushState("", "", url);
+    window.location.reload(true);
+  };
+
+  const renderedSource = <TrustedSourceView publicKey={publicKey} />;
+
+  if (short) {
+    return (
+      <Card sx={{ m: 1, p: 3, cursor: "pointer" }} onClick={onClick}>
+        <Box sx={{ display: "flex" }}>
+          <Box>
+            <QRCode value={url} size={100} />
+          </Box>
+          <Box sx={{ marginLeft: 2 }}>
+            <IconText label="Vehicle Number" text={payload.vehicleNumber} />
+            <IconText label="Priority" text={payload.priority} />
+            <IconText
+              label="Time Expiry"
+              text={TimeX.formatTime(payload.timeExpiryUT)}
+              color={color}
+            />
+          </Box>
+        </Box>
+        {renderedSource}
+      </Card>
+    );
+  } else {
+    return (
+      <Stack spacing={1} sx={{ m: 1, p: 3 }}>
+        <Typography variant="h4">Token</Typography>
+        {renderedSource}
+        <QRCode value={url} />
+        <Box>
+          <LabelledText label="Vehicle Number" text={payload.vehicleNumber} />
+          <LabelledText label="Priority" text={payload.priority} />
+          <LabelledText
+            label="Time Expiry"
+            text={TimeX.formatTime(payload.timeExpiryUT)}
+            color={color}
+          />
+
+          <LabelledText
+            label="Time Created"
+            text={TimeX.formatTime(payload.timeCreatedUT)}
+          />
+          <LabelledText label="Public Key" text={publicKey} />
+        </Box>
+      </Stack>
+    );
+  }
 }
