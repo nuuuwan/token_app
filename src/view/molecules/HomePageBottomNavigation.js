8901745fe67bd3f03d36523b183f83b43f2e6a4e
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-30 12:08:48 +0530

    Hide buttons based on mode

--- src/view/molecules/HomePageBottomNavigation.js
+++ src/view/molecules/HomePageBottomNavigation.js
@@ -7,24 +7,28 @@ import KeyIcon from "@mui/icons-material/Key";
 import NoteAddIcon from "@mui/icons-material/NoteAdd";
 import QrCodeScannerIcon from "@mui/icons-material/QrCodeScanner";
 import ViewListIcon from "@mui/icons-material/ViewList";
-
+import URLContext from "../../nonview/base/URLContext";
+import Condition from "../../view/atoms/Condition";
 export default function HomePageBottomNavigation({ onClickOpenPage }) {
+  const context = URLContext.getContext();
   return (
     <Paper
       sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
       elevation={3}
     >
       <BottomNavigation showLabels>
-        <BottomNavigationAction
-          label="Crypto Keys"
-          icon={<KeyIcon />}
-          onClick={() => onClickOpenPage("cryptoKeys")}
-        />
-        <BottomNavigationAction
-          label="Create Token"
-          icon={<NoteAddIcon />}
-          onClick={() => onClickOpenPage("createToken")}
-        />
+        <Condition condition={context.mode === "issuer"}>
+          <BottomNavigationAction
+            label="Crypto Keys"
+            icon={<KeyIcon />}
+            onClick={() => onClickOpenPage("cryptoKeys")}
+          />
+          <BottomNavigationAction
+            label="Create Token"
+            icon={<NoteAddIcon />}
+            onClick={() => onClickOpenPage("createToken")}
+          />
+        </Condition>
         <BottomNavigationAction
           label="Scan Token"
           icon={<QrCodeScannerIcon />}
