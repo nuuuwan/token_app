07f2b7ad1f920b8c8a8bb6739b3ce1f0d798ff80
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-30 14:30:16 +0530

    Added AppColors

---
+++ src/view/_constants/AppColors.js
@@ -0,0 +1,9 @@
+import SriLankaColors from "../../view/_constants/SriLankaColors";
+
+const AppColors = {
+  Issuer: SriLankaColors.Tamil,
+  Receiver: SriLankaColors.Muslim,
+  QRCode: SriLankaColors.Sinhala,
+};
+
+export default AppColors;
