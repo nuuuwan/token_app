32e3e0624f5a9545a041a1d019e05957ada2a9af
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-30 13:49:23 +0530

    Added CONFIG_LIST

--- src/view/molecules/HomePageBottomNavigation.js
+++ src/view/molecules/HomePageBottomNavigation.js
@@ -3,6 +3,7 @@ import React from "react";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import Paper from "@mui/material/Paper";
+import Tooltip from "@mui/material/Tooltip";
 import KeyIcon from "@mui/icons-material/Key";
 import NoteAddIcon from "@mui/icons-material/NoteAdd";
 import QrCodeScannerIcon from "@mui/icons-material/QrCodeScanner";
@@ -11,41 +12,58 @@ import ViewListIcon from "@mui/icons-material/ViewList";
 import { t } from "../../nonview/base/I18N";
 import URLContext from "../../nonview/base/URLContext";
 
-import Condition from "../../view/atoms/Condition";
+const CONFIG_LIST = [
+  {
+    label: "Crypto Keys",
+    Icon: KeyIcon,
+    page: "cryptoKeys",
+    showInOnlyAdminMode: true,
+  },
+  {
+    label: "Create Token",
+    Icon: NoteAddIcon,
+    page: "createToken",
+    showInOnlyAdminMode: true,
+  },
+  {
+    label: "Scan Token",
+    Icon: QrCodeScannerIcon,
+    page: "scanToken",
+    showInOnlyAdminMode: false,
+  },
+  {
+    label: "My Tokens",
+    Icon: ViewListIcon,
+    page: "tokenList",
+    showInOnlyAdminMode: false,
+  },
+];
 
 export default function HomePageBottomNavigation({ onClickOpenPage }) {
   const context = URLContext.getContext();
 
+  const isAdminMode = context.mode === "issuer";
   return (
     <Paper
       sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
       elevation={3}
     >
-      <BottomNavigation showLabels>
-        <Condition condition={context.mode === "issuer"}>
-          <BottomNavigationAction
-            label={t("Crypto Keys")}
-            icon={<KeyIcon />}
-            onClick={() => onClickOpenPage("cryptoKeys")}
-            showLabel
-          />
-          <BottomNavigationAction
-            label={t("Create Token")}
-            icon={<NoteAddIcon />}
-            onClick={() => onClickOpenPage("createToken")}
-            showLabel
-          />
-        </Condition>
-        <BottomNavigationAction
-          label={t("Scan Token")}
-          icon={<QrCodeScannerIcon />}
-          onClick={() => onClickOpenPage("scanToken")}
-        />
-        <BottomNavigationAction
-          label={t("My Tokens")}
-          icon={<ViewListIcon />}
-          onClick={() => onClickOpenPage("tokenList")}
-        />
+      <BottomNavigation>
+        {CONFIG_LIST.map(function (config) {
+          const key = "button-" + config.page;
+          if (config.showInOnlyAdminMode && !isAdminMode) {
+            return null;
+          }
+          return (
+            <Tooltip title={t(config.label)}>
+              <BottomNavigationAction
+                key={key}
+                icon={<config.Icon />}
+                onClick={() => onClickOpenPage(config.page)}
+              />
+            </Tooltip>
+          );
+        })}
       </BottomNavigation>
     </Paper>
   );
