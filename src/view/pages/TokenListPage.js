ee2a6178ab04b8d715f1b45447ab3af11a7de437
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-30 08:53:49 +0530

    Added You have no tokens stored on this device.

--- src/view/pages/TokenListPage.js
+++ src/view/pages/TokenListPage.js
@@ -1,28 +1,43 @@
 import { Component } from "react";
+
+import Alert from "@mui/material/Alert";
+import Paper from "@mui/material/Paper";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
+
 import Crypto from "../../nonview/base/Crypto";
-import Paper from "@mui/material/Paper";
 
 export default class TokenListPage extends Component {
-  render() {
+  renderTokenList() {
     const tokenUrlList = Crypto.getTokenUrlList();
+
+    if (!tokenUrlList || tokenUrlList.length === 0) {
+      return (
+        <Alert severity="warning">
+          You have no tokens stored on this device.
+        </Alert>
+      );
+    }
+
+    return tokenUrlList.map(function (tokenUrl, iToken) {
+      const onClick = function () {
+        window.history.pushState("", "", tokenUrl);
+        window.location.reload(true);
+      };
+      return (
+        <Paper key={"token-" + iToken} sx={{ m: 1, p: 1 }}>
+          <Typography href={tokenUrl} onClick={onClick}>
+            Token + {iToken}
+          </Typography>
+        </Paper>
+      );
+    });
+  }
+  render() {
     return (
       <Stack spacing={1} sx={{ m: 1, p: 3 }}>
         <Typography variant="h4">My Tokens</Typography>
-        {tokenUrlList.map(function (tokenUrl, iToken) {
-          const onClick = function () {
-            window.history.pushState("", "", tokenUrl);
-            window.location.reload(true);
-          };
-          return (
-            <Paper key={"token-" + iToken} sx={{ m: 1, p: 1 }}>
-              <Typography href={tokenUrl} onClick={onClick}>
-                Token + {iToken}
-              </Typography>
-            </Paper>
-          );
-        })}
+        {this.renderTokenList()}
       </Stack>
     );
   }
