055987b28a1ecf474b9cbb87cc611168eab6b0bc
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-30 11:51:34 +0530

    Added functionality and styles

--- src/view/pages/ScanTokenPage.js
+++ src/view/pages/ScanTokenPage.js
@@ -1,21 +1,29 @@
 import { Component } from "react";
+import { QrReader } from "react-qr-reader";
 
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
-import { QrReader } from "react-qr-reader";
+
 import URLContext from "../../nonview/base/URLContext";
+
+const STYLE_QR_READER = {
+  width: "100%",
+};
+
+const STYLE_QR_READER_CONTAINER = {
+  border: "solid",
+  backgroundColor: "#eee",
+};
 
 export default class ScanToken extends Component {
   onResult(result, error) {
-    if (error) {
-      console.error(error);
-    } else if (result) {
+    if (result && result.text) {
       const url = result.text;
       const context = URLContext.urlToContext(url);
 
       if (context.token && context.page === "viewToken") {
-        window.history.pushState("", "", url);
-        window.location.reload(true);
+        console.debug("Openning URL: " + url);
+        URLContext.open(url);
       }
     }
   }
@@ -26,7 +34,8 @@ export default class ScanToken extends Component {
         <Typography variant="h4">Scan Token</Typography>
         <QrReader
           onResult={this.onResult.bind(this)}
-          style={{ width: "100%" }}
+          style={STYLE_QR_READER}
+          containerStyle={STYLE_QR_READER_CONTAINER}
         />
       </Stack>
     );
