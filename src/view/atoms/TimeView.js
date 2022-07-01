5278593fbce115c0cb599de3a8beea122db5afaa
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-07-01 14:16:48 +0530

    Time View

---
+++ src/view/atoms/TimeView.js
@@ -0,0 +1,13 @@
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import TimeX from "../../nonview/base/TimeX";
+export default function TimeView({ ut }) {
+  return (
+    <Stack direction="column" spacing={-1}>
+      <Typography variant="subtitle1">{TimeX.getHumanTime(ut)}</Typography>
+      <Typography variant="caption">{TimeX.formatTime(ut)}</Typography>
+    </Stack>
+  );
+}
+
+// import TimeView from "../../view/atoms/TimeView";
