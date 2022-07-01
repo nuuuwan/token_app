1be2472dccdc10385ee7b3c91f5e3ed6f090df33
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-07-01 13:50:09 +0530

    Added InValid and Valid colors; Updated Primary + Secondary

--- src/view/_constants/AppColors.js
+++ src/view/_constants/AppColors.js
@@ -1,12 +1,15 @@
 import SriLankaColors from "../../view/_constants/SriLankaColors";
 
 const AppColors = {
-  Primary: SriLankaColors.Sinhala,
-  Secondary: SriLankaColors.Tamil,
+  Primary: "#000",
+  Secondary: "#888",
 
   Issuer: SriLankaColors.Tamil,
   Receiver: SriLankaColors.Sinhala,
   QRCode: SriLankaColors.Muslim,
+
+  Invalid: "rgba(128,0,0,0.1)",
+  Valid: "rgba(0,128,0,0.1)",
 };
 
 export default AppColors;
