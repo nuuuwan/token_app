53cc1d3d5ee88662f6953c43eebd4d86946330bc
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-07-01 17:34:39 +0530

    Updated translation script

--- translate.sh
+++ translate.sh
@@ -1,3 +1,5 @@
+source /Users/nuwan.senaratna/.bashrc;
+
 DIR_I18N=/Users/nuwan.senaratna/Dropbox/_CODING/py/i18n
 DIR_APP_PARENT=/Users/nuwan.senaratna/Not.Dropbox/_CODING/js_react
 
@@ -7,5 +9,8 @@ cd $DIR_I18N
 python3  build.py "$DIR_APP_PARENT/$APP_NAME/src/nonview/base"
 
 cd $DIR_APP_PARENT/$APP_NAME
+
+jlint
+
 git add src/nonview/base/DICTIONARY.*
 git commit -m "I18N"
